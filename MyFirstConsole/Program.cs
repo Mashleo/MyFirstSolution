5ebfd64e080e2824363fa8a2cf75c550442c58cb
leonid.mashlyakevich <leonid.mashlyakevich@gmail.com>
2022-02-17 11:01:00 +0300

    Add project files.

---
+++ MyFirstConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myFifrstInt = 10;
+
+            Console.WriteLine(myFifrstInt);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            decimal e = 1.414213562373095048M;
+
+            Console.WriteLine("Константа Пи = " + pi);
+            Console.WriteLine("Константа Пифагора = " + e);
+            Console.WriteLine("Константа Пи которая встроеная в С# = " + Math.PI);
+        }
+    }
+}
